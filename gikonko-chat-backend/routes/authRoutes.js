2345b4881e8a376eac985954916586a95616a4a1
nietiene <niyomugaboetiene53@gmail.com>
2025-07-03 13:54:52 -0700

    first commit of gikonko-tss-chat

--- gikonko-chat-backend/routes/authRoutes.js
+++ gikonko-chat-backend/routes/authRoutes.js
@@ -1,2 +1,8 @@
 import express from "express";
 import { register, login, logout, getProfile } from "../controllers/authController";
+const router = express.Router();
+
+router.post('/register', register);
+router.post('/login', login);
+router.post('/logout', logout);
+router.get('/profile', getProfile);
