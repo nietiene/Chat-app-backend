0b4470206b4ec7c774ef76088c8c43d2597c3b86
nietiene <niyomugaboetiene53@gmail.com>
2025-07-03 09:59:52 -0700

    first commit of gikonko-tss-chat

--- gikonko-chat-backend/app.js
+++ gikonko-chat-backend/app.js
@@ -12,4 +12,17 @@ dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST'],
+        credentials: true
+    }
+});
+
+app.use(cors({
+    origin:'http://localhost:5173',
+    credentials: true
+}));
+
+app.use(express.json());
