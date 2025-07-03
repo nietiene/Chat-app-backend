b0a5e2d2a4f6d3f92e4a00fdeefc4d58c050b78e
nietiene <niyomugaboetiene53@gmail.com>
2025-07-03 13:43:00 -0700

    first commit of gikonko-tss-chat

--- gikonko-chat-backend/controllers/authController.js
+++ gikonko-chat-backend/controllers/authController.js
@@ -1,6 +1,14 @@
 import { createUser, findUserByPhone } from "../models/userModel.js";
 import bcrypt from "bcrypt";
 
-export async function register(req, res) {
-   
+export async function register (req, res) {
+     try {
+        const { name, phone, password } = req.body;
+        const existingUser = await findUserByPhone(phone);
+
+            if (existingUser) {
+                return res.status(400).json({ message: 'User already exist'})
+            }
+        
+     }
 }
