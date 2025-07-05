335d077139c7242b8686fff5606e0571c301f491
nietiene <niyomugaboetiene53@gmail.com>
2025-07-05 13:01:23 -0700

    commit of gikonko-tss-chat

---
+++ gikonko-chat-backend/routes/messageRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { saveMessage, getMessageBetweenUsers } from "../models/messageModel.js";
+
+const router = express.Router();
+
+router.get('/:user1/:user2', async (req, res) => {
+    const { user1, user2 } = req.params;
+
+    try {
+        const messages = await getMessageBetweenUsers(user1, user2);
+        res.json(messages);
+    } catch(error) {
+        res.status(500).json({ error: 'Failed to get message' });
+    }
+});
+
