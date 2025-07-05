de3fe2271d63c547e3d843dba32665d42bc7434b
nietiene <niyomugaboetiene53@gmail.com>
2025-07-05 12:57:22 -0700

    commit of gikonko-tss-chat

--- gikonko-chat-backend/models/messageModel.js
+++ gikonko-chat-backend/models/messageModel.js
@@ -6,7 +6,21 @@ export function saveMessage(sender, receiver, content, image = null) {
         db.query(query, [sender, receiver, content, image], (err, result) => {
             if (err) return reject(err);
 
-            resolve(resolve);
+            resolve(result);
         })
+    })
+}
+
+
+export function getMessageBetweenUsers(user1, user2) {
+    return new Promise((resolve, reject) => {
+        const query = `
+                     SELECT * FROM messages WHERE (sender_id = ? AND receiver_id = ?) OR (sender_id = ? AND receiver_id = ?)
+                     ORDER BY created_at ASC
+                     `;
+      db.query(query, [user1, user2, user2, user1], (err, result) => {
+        
+      });
+
     })
 }
