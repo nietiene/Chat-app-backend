db524ff19d44a1ff0a10b4ece80f35759b222f4b
nietiene <niyomugaboetiene53@gmail.com>
2025-07-05 07:19:35 -0700

    commit of gikonko tss chat

--- gikonko-chat-backend/app.js
+++ gikonko-chat-backend/app.js
@@ -71,7 +71,7 @@ io.on('connection', (socket) => {
     })
     socket.on("disconnect", () => {
         for (const username in users) {
-            if (users[username] = users[to]) {
+            if (users[username] = socket.id) {
                 delete users[username];
                 io.emit("userList", Object.keys(users));
                 break;
