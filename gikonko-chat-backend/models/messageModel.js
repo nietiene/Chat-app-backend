7efa01bbcde3baec53faaa14a8c58c7ab478365e
nietiene <niyomugaboetiene53@gmail.com>
2025-07-06 07:55:47 -0700

    commit of gikonko-tss-chat

--- gikonko-chat-backend/models/messageModel.js
+++ gikonko-chat-backend/models/messageModel.js
@@ -2,23 +2,14 @@ import pool from "./db.js";
 
 export async function saveMessage(sender, receiver, content, image = "") {
     const [result] = await pool.query(
-        "INSERT INTO messages (sender_id, receiver_id, content, image)"
-    )
+        "INSERT INTO messages (sender_id, receiver_id, content, image) VALUES(?, ?, ?, ?)",
+        [sender, receiver, content, image]
+    );
+    return result;
     
 }
 
 
-export function getMessageBetweenUsers(user1, user2) {
-    return new Promise((resolve, reject) => {
-        const query = `
-                     SELECT * FROM messages WHERE (sender_id = ? AND receiver_id = ?) OR (sender_id = ? AND receiver_id = ?)
-                     ORDER BY created_at ASC
-                     `;
-      pool.query(query, [user1, user2, user2, user1], (err, result) => {
-        if (err) reject(err);
+export async function getMessageBetweenUsers(user1, user2) {
 
-        resolve(result);
-      });
-
-    })
 }
