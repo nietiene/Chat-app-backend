0641b73070b1c51c90eb8047e28cb5baf4aa60fa
nietiene <niyomugaboetiene53@gmail.com>
2025-07-06 17:29:53 -0700

    commit of gikonko-tss-chat

--- gikonko-chat-backend/routes/userRoutes.js
+++ gikonko-chat-backend/routes/userRoutes.js
@@ -1,8 +1,19 @@
 import { getAllUsers  } from "../controllers/userController.js";
 import express from "express";
+import multer from "multer";
+import path from "path";
 
 const router = express.Router();
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "/uploads/");
+    },
+    filename: (req, file, cb) => {
+        const ext = path.extname(file.originalname);
+        const 
+    }
+})
 
 router.get('/', getAllUsers);
 
