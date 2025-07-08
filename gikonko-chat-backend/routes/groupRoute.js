34fa35f5a7de77e5f07a1752de505c1a71cfde73
nietiene <niyomugaboetiene53@gmail.com>
2025-07-08 16:50:13 -0700

    commit of gikonko-tss-chat

--- gikonko-chat-backend/routes/groupRoute.js
+++ gikonko-chat-backend/routes/groupRoute.js
@@ -5,11 +5,9 @@ import { getGroupMessages, sendGroupMessage } from "../controllers/groupControll
 const router = express.Router();
 
 router.post('/', isAuthenticated, createGroup);
-
 router.get('/my', isAuthenticated, getMyGroup);
 
 router.get('/group-messages/:g_id', getGroupMessages);
-
 router.post('/group-messages', sendGroupMessage);
 
 export default router;
