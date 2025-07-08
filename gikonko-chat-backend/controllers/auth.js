3227846edf440e59a30c479b1b1e2d798aa8e65e
nietiene <niyomugaboetiene53@gmail.com>
2025-07-08 14:07:31 -0700

    commit of gikonko-tss-chat

---
+++ gikonko-chat-backend/controllers/auth.js
@@ -0,0 +1,8 @@
+export function isAuthenticated (req, res, next) {
+    if (req.session && req.session.user) {
+        req.user = req.session.user;
+        next(); 
+    } else {
+        res.status(401).json({ message: 'Unauthorized' });
+    }
+}
