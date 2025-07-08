c1907a7ca87b1a652e780272aaeec10d36691065
nietiene <niyomugaboetiene53@gmail.com>
2025-07-08 13:51:59 -0700

    commit of gikonko-tss-chat

--- gikonko-chat-backend/routes/groupRoute.js
+++ gikonko-chat-backend/routes/groupRoute.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { createGroup, getMyGrouP } from "../controllers/groupController.js";
+import { createGroup, getMyGroup } from "../controllers/groupController.js";
 import { login } from "../controllers/authController.js";
 
 const router = express.Router();
+
+router.post('/', login, createGroup);
+
+router.get('/my', login, getMyGroup);
+
+export default router;
