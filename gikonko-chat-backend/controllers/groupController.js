72dee6637c78cab42c2105a901f1e2bc707ab9bc
nietiene <niyomugaboetiene53@gmail.com>
2025-07-07 20:18:32 -0700

    commit of gikonko-tss-chat

---
+++ gikonko-chat-backend/controllers/groupController.js
@@ -0,0 +1,10 @@
+import db from "../models/db.js";
+
+export async function createGroup(req, res) {
+    const { group_name } = req.body;
+    const userId = req.session.user_id;
+
+    if (!group_name || !userId) {
+        return res.status(400).json({ error: "Missing group name or user not logged in" })
+    }
+}
