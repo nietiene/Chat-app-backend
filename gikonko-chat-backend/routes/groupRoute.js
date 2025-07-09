112a4acb0b78ad9341f330ee1e49063c7aa4b5a5
nietiene <niyomugaboetiene53@gmail.com>
2025-07-09 08:38:55 -0700

    commit of gikonko-tss-chat

--- gikonko-chat-backend/routes/groupRoute.js
+++ gikonko-chat-backend/routes/groupRoute.js
@@ -2,6 +2,8 @@ import express from "express";
 import { createGroup, getMyGroup } from "../controllers/groupController.js";
 import { isAuthenticated } from "../controllers/auth.js";
 import { getGroupMessages, sendGroupMessage } from "../controllers/groupController.js";
+import db from "../models/db.js";
+
 const router = express.Router();
 
 router.post('/', isAuthenticated, createGroup);
@@ -10,4 +12,22 @@ router.get('/my', isAuthenticated, getMyGroup);
 router.get('/group-messages/:g_id', isAuthenticated, getGroupMessages);
 router.post('/:g_id/messages', isAuthenticated, sendGroupMessage);
 
+router.get('/group_members/:g_id', async (req, res) => {
+    const groupId = req.params.g_id;
+
+    try {
+        const [rows] = await db.query (
+            `SELECT gm.user_id, u.name
+            FROM group_members gm 
+            JOIN user u ON u.user_id = gm.user_id
+            WHERE gm.g_id = ?`,
+            [groupId]
+        );
+
+        res.json(rows);
+    } catch (error) {
+        console.error("Error fetching group members", error);
+        res.status(500).json({ error: 'Failed to fetch group members' });
+    }
+})
 export default router;
