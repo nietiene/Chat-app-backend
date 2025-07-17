64fa14a5030463fd4439a196939117ba2dfdb337
nietiene <niyomugaboetiene53@gmail.com>
2025-07-17 15:57:21 -0700

    commit of gikonko-tss-chat

---
+++ gikonko-chat-backend/routes/notificationRoute.js
@@ -0,0 +1,24 @@
+import express from "express";
+import pool from "../models/db";
+
+const router = express.Router();
+
+router.get('/notification/:user_id', async (req, res) => {
+    const { user_id } = req.params;
+
+    try {
+        const [rows] = await pool.query(
+            'SELECT * FROM notifications WHERE receiver_id = ? AND is_read = 0 ORDER BY created_at DESC',
+            [user_id]
+        );
+
+        res.json(rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json('Error fetching notifications');
+    }
+})
+
+
+// Mark as readed
+router.post('/notification/mark-read/user_id')
