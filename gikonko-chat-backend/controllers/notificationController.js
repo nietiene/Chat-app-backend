34d4c2330feaab2a2eb40eb433a46065187df7a7
nietiene <niyomugaboetiene53@gmail.com>
2025-07-17 16:43:06 -0700

    commit of gikonko-tss-chat

--- gikonko-chat-backend/controllers/notificationController.js
+++ gikonko-chat-backend/controllers/notificationController.js
@@ -3,6 +3,12 @@ import db from "../models/db.js";
 export const createNotification = async (req, res) => {
     const {	receiver_id, sender_id,	content, is_read, created_at } = req.body;
     const io = req.app.get('io');
+
+    try {
+        const [result] = await db.query(
+            'INSERT INTO notfications (receiver_id, sender_id, type, '
+        )
+    }
     
 
 }
