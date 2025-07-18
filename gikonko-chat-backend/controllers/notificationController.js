dd5bb3e5f5e20b77e3d3f15dd84e7065371ddf03
nietiene <niyomugaboetiene53@gmail.com>
2025-07-17 17:33:42 -0700

    commit of gikonko-tss-chat

--- gikonko-chat-backend/controllers/notificationController.js
+++ gikonko-chat-backend/controllers/notificationController.js
@@ -1,26 +1,28 @@
 import db from "../models/db.js";
 
 export const createNotification = async (req, res) => {
-    const {	receiver_id, sender_id,	content, is_read = 0 } = req.body;
+    const {	receiver_id, sender_id,	type, content } = req.body;
     const io = req.app.get('io');
 
     try {
         const created_at = new Date();
         const [result] = await db.query(
-            'INSERT INTO notfications (receiver_id, sender_id, content, is_read, created_at) VALUES(?, ?, ?, ?, ?)',
-            [receiver_id, sender_id, content, is_read, created_at]
+            'INSERT INTO notfications (receiver_id, sender_id, type, content, ) VALUES(?, ?, ?, ?)',
+            [receiver_id, sender_id, type, content,]
         );
 
-        const notification = {
-            id: result.insertId,
-            receiver_id,
-            sender_id,
-            content,
-            is_read,
-            created_at
-        };
+        const receiverSocketId = req.app.get('onlineUsers')?.[receiver_id];
 
-        io.to(`user_${receiver_id}`).emit('new_notification', notification);
+        if (receiverSocketId) {
+            io.to(receiverSocketId).emit('notification', {
+               id: result.insertId,
+               receiver_id,
+               sender_id,
+               content,
+               is_read: 0,
+               created_at: new Date()
+            })
+        }
 
         res.status(201).json({ success: true, notification });
     } catch (error) {
