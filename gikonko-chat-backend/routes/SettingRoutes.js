b6e09ef84fc18d569603d66b07b6021b93ffd044
nietiene <niyomugaboetiene53@gmail.com>
2025-07-19 14:17:04 -0700

    commit of gikonko-tss-chat

--- gikonko-chat-backend/routes/SettingRoutes.js
+++ gikonko-chat-backend/routes/SettingRoutes.js
@@ -5,10 +5,14 @@ import bcrypt from "bcrypt";
 const router = express.Router();
 
 router.get('/me', async (req, res) => {
-    const userId = req.sessio.user_id;
+    const userId = req.session.user.id;
+
     if (!userId) return res.status(401).json({ message: 'Unauthorized' });
 
     try {
+        const [rows] = await pool.query('SELECT name, phone, FROM user WHERE user_id = ?', [userId]);
+        if (rows.length === 0) return res.status(404).json({ message: 'User not found' });
         
+
     }
 })
