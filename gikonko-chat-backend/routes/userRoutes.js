abff97b257532b3fc090a65fb375b02121d34258
nietiene <niyomugaboetiene53@gmail.com>
2025-07-19 07:43:18 -0700

    commit of gikonko-tss-chat

--- gikonko-chat-backend/routes/userRoutes.js
+++ gikonko-chat-backend/routes/userRoutes.js
@@ -81,7 +81,7 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:user_id', async (req, res) => {
     const userId = req.params.user_id;
 
     try {
