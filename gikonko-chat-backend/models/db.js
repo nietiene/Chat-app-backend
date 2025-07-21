b044b00ce70a150d8d73fe969efadad836b10c3f
nietiene <niyomugaboetiene53@gmail.com>
2025-07-21 14:43:39 -0700

    commit of gikonko-tss-chat

--- gikonko-chat-backend/models/db.js
+++ gikonko-chat-backend/models/db.js
@@ -9,7 +9,7 @@ const pool = mysql.createPool({
     database: process.env.DB_NAME,
     waitForConnections: true, // when connection is full means it exceed to 20 connection is to wait until connection become available istead of fail
     connectionLimit: 10, // maximum number of connection
-    queueLimit: 0 // 
+    queueLimit: 0 // helps to avoid overloading your server with too many queued requests
 
 });
 
