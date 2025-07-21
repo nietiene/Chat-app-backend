d65e317df541aa807c63a1f519b73014446b5b42
nietiene <niyomugaboetiene53@gmail.com>
2025-07-21 14:32:23 -0700

    commit of gikonko-tss-chat

--- gikonko-chat-backend/controllers/notificationController.js
+++ gikonko-chat-backend/controllers/notificationController.js
@@ -1,12 +1,14 @@
 import db from "../models/db.js";
 
 
-let io = null;
-let onlineUsers = {};
+let io = null; // holds your socket.IO server instance; we it to null because it is not yet set
+let onlineUsers = {}; // this objects helps to map user_Id and their socket_id
 
+
+// this function initialize notification servises by setting up io and online users
 export function setupNotificationService (socketIO, users) {
-    io = socketIO,
-    onlineUsers = users;
+    io = socketIO, // main socket.IO server you pass in
+    onlineUsers = users; // mapping connected users
 }
 export const sendNotification = async ({ receiver_id, sender_id, type, content }) => {
     try {
@@ -18,9 +20,13 @@ export const sendNotification = async ({ receiver_id, sender_id, type, content }
         );
 
 
-        const socketId = onlineUsers[receiver_id];
+        const socketId = onlineUsers[receiver_id]; // gets socket IDs of who should receive notification
 
-        if (socketId && io) {
+        if (socketId && io) { // if we get those socket ID and socket.IO server is set  run those following code
+
+            // this sends real time notification event for only olnine user
+            // to() is used to target spacific socket or group of sockets
+            // emit() is used to send an event
             io.to(socketId).emit('notification', {
                id: result.insertId,
                receiver_id,
